aa7a2a964b62a4202ea71602e4761bbdfb1d7e5d
Alex Blackman <alexander.blackman@gmail.com>
2021-02-01 14:30:37 +1100

    Add project files.

---
+++ HFC_ComicCrazy/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HFC_ComicCrazy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // IEnumerable<Comic> mostExpensive =
+                var mostExpensive = 
+                from comic in Comic.Catalogue
+                where Comic.Prices[comic.Issue] > 500m
+                orderby -Comic.Prices[comic.Issue]
+                select comic;
+               
+
+            foreach (Comic comic in mostExpensive)
+                Console.WriteLine($"{comic} is worth {Comic.Prices[comic.Issue]:c}");
+
+        }
+    }
+}
